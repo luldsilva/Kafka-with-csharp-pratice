051f83c484893486ff41b0ad97bed96c429cdeb3
Lucas Miha <lucaslisilva@outlook.com>
2022-07-21 16:41:31 -0300

    Add project files.

---
+++ Kafka.Consumer.Handler/Handlers/MessageHandler.cs
@@ -0,0 +1,56 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kafka.Consumer.Handler.Handlers
+{
+    public class MessageHandler : IHostedService
+    {
+        private readonly ILogger _logger;
+        public MessageHandler(ILogger<MessageHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "MessageHandler",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using(var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+            {
+                c.Subscribe("NewOrder");
+                var cts = new CancellationTokenSource();
+
+                try
+                {
+                    while (true)
+                    {
+                        var message = c.Consume(cts.Token);
+                        _logger.LogInformation($"Mesangem: {message.Value} recebida de {message.TopicPartitionOffset}");
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    c.Close();
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Kafka.Producer.API/Controllers/Producer.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace Kafka.Producer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Producer : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(string),201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public IActionResult Post([FromQuery] string msg)
+        {
+            return Created("", SendMessageByKafka(msg));
+        }
+
+        private string SendMessageByKafka(string message)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            using(var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var sendResult = producer.ProduceAsync("NewOrder", new Message<Null, string> { Value = message })
+                        .GetAwaiter()
+                        .GetResult();
+                    return $"Mensagem '{sendResult.Value}' de '{sendResult.TopicPartitionOffset}'";
+                } 
+                catch(ProduceException<Null, string> ex)
+                {
+                    Console.WriteLine($"Delivery failed: {ex.Error.Reason}");
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
